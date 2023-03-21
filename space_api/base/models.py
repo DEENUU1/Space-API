febcae7b1e96c51c38af2c5538d239fa05beebdc
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-03-21 14:42:52 +0100

    Models | System | This is just a simple model which allows to store a planet systems Later will have more features It is relationally connected to the galaxy

--- space_api/base/models.py
+++ space_api/base/models.py
@@ -8,3 +8,9 @@ class Galaxy(models.Model):
         return self.name
 
 
+class System(models.Model):
+    name = models.CharField(max_length=120)
+    galaxy = models.ForeignKey(Galaxy, on_delete=models.CASCADE, related_name='systems')
+    
+    def __str__(self):
+        return self.name
