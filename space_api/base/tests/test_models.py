d88acb4738aaae2770179361bb0004a4ba7bd25a
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-03-21 22:50:30 +0100

    Test | System model | Test that is testing the System model with a correct values and the relation of this model with Galaxy model

--- space_api/base/tests/test_models.py
+++ space_api/base/tests/test_models.py
@@ -20,6 +20,19 @@ class TestModels(TestCase):
             size="100 ly"
         )
 
+        self.system1 = System.objects.create(
+            id=1,
+            name="Test System",
+            description="Simple text",
+            age="700 billion years",
+            mass="8000 billion tones",
+            number_of_stars=1,
+            star="Sun",
+            satellites="Moon",
+            number_of_satellites=1,
+            galaxy=self.galaxy1
+        )
+
     def test_galaxy_model(self) -> None:
         """
         Test that the Galaxy object has the correct attributes
@@ -28,3 +41,18 @@ class TestModels(TestCase):
         self.assertEqual(self.galaxy1.id, 1)
         self.assertEqual(self.galaxy1.description, "Simple text")
         self.assertEqual(self.galaxy1.number_of_stars, 10)
+
+    def test_system_model(self) -> None:
+        """
+        Test that the System object has the correct attributes
+        """
+        self.assertEqual(self.system1.name, "Test System")
+        self.assertEqual(self.system1.description, "Simple text")
+        self.assertEqual(self.system1.number_of_stars, 1)
+        self.assertEqual(self.system1.number_of_satellites, 1)
+
+    def test_system_relation_with_galaxy(self) -> None:
+        """
+        Test that the System object is related to the correct Galaxy object
+        """
+        self.assertEqual(self.system1.galaxy, self.galaxy1)
