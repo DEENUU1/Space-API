3cfd20d1c9b1981ea6a3c0316464dc8e3c898460
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-03-22 16:15:14 +0100

    Feature | User and auth | View which allows user to log in using username and password Changes inside Signup form and view which allows to add username

--- space_api/users/views.py
+++ space_api/users/views.py
@@ -5,14 +5,15 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 
 class SignUpView(View):
     """
     This view allows user to create a new account
     """
-    form_class = CreateUserForm
-    template_name = 'register.html'
+    FORM_CLASS = CreateUserForm
+    TEMPLATE_NAME = 'registration/register.html'
 
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated:
@@ -20,15 +21,19 @@ class SignUpView(View):
         return super().dispatch(request, *args, **kwargs)
 
     def get(self, request, *args, **kwargs):
-        form = self.form_class
-        return render(request, self.template_name, {'form': form})
+        form = self.FORM_CLASS
+        return render(request, self.TEMPLATE_NAME, {'form': form})
 
     def post(self, request, *args, **kwargs):
-        form = self.form_class(request.POST)
+        form = self.FORM_CLASS(request.POST)
 
         if User.objects.filter(email=form.data['email']).exists():
             messages.error(request,
                            'This email address already exist')
+
+        if User.objects.filter(username=form.data['username']).exists():
+            messages.error(request,
+                           'This username already exist')
 
         if form.is_valid():
             form.save()
@@ -39,5 +44,32 @@ class SignUpView(View):
             messages.error(request,
                            'Account creation failed')
 
-        return render(request, self.template_name, {'form': form})
+        return render(request, self.TEMPLATE_NAME, {'form': form})
 
+
+class SignInView(LoginView):
+    """
+    This view allows users to log in.
+    """
+    TEMPLATE_NAME = 'login.html'
+
+    def get(self, request):
+        if request.user.is_authenticated:
+            return redirect('/')
+        return render(request, self.TEMPLATE_NAME)
+
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request,
+                            username=username,
+                            password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request,
+                          'Try again')
+            return render(request, self.TEMPLATE_NAME)
