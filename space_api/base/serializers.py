fd44ddaad1b930d5e2e34b68c82fda80702ffa12
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-03-23 18:10:23 +0100

    Feature | System serializer | Serializer for System model which includes the all available fields

--- space_api/base/serializers.py
+++ space_api/base/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Planet
+from .models import Planet, System
 
 
 class PlanetSerializer(serializers.ModelSerializer):
@@ -11,4 +11,16 @@ class PlanetSerializer(serializers.ModelSerializer):
     """
     class Meta:
         model = Planet
+        fields = '__all__'
+
+
+class SystemSerializer(serializers.ModelSerializer):
+    """
+    A serializer class for the System model
+    Attributes:
+        Meta (class): A nested class which specifies the model
+        and fields to be use fot serialization.
+    """
+    class Meta:
+        model = System
         fields = '__all__'
