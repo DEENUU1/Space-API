c511f15e23d857febf968fab4fda5ba12dd99ddb
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-03-23 18:13:22 +0100

    Feature | SystemList | View proviedes a read-only list of all the System objects. Inherits from the ListAPIView class provied by Django Rest Framework endpoint for this view looks like this 'api/systems/?api_key=<<>>'

--- space_api/base/urls.py
+++ space_api/base/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import getRoutes, PlanetList
+from .views import getRoutes, PlanetList, SystemList
 
 app_name = 'base'
 
 urlpatterns = [
     path('', getRoutes, name='get-routes'),
     path('planets/', PlanetList.as_view(), name='planet-list'),
+    path('systems/', SystemList.as_view(), name='system-list'),
 
 ]
--- space_api/base/views.py
+++ space_api/base/views.py
@@ -1,9 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import generics, permissions
-from rest_framework.authentication import TokenAuthentication
-from .models import Planet
-from .serializers import PlanetSerializer
+from .models import Planet, System
+from .serializers import PlanetSerializer, SystemSerializer
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.authtoken.models import Token
@@ -17,6 +16,12 @@ def getRoutes(request):
     routes = [
         {
             'Endpoint': 'api/planets/',
+            'method': 'GET',
+            'body': None,
+            'description': '?api_key=<<YOUR API KEY>>'
+        },
+        {
+            'Endpoints': 'api/systems',
             'method': 'GET',
             'body': None,
             'description': '?api_key=<<YOUR API KEY>>'
@@ -67,3 +72,30 @@ class PlanetList(generics.ListAPIView):
             return Planet.objects.all()
         else:
             return None
+
+
+class SystemList(generics.ListAPIView):
+    """
+    This view provides a read-only list of all the Planet objects.
+    Inherits from the ListAPIView class provided by DRF
+    Attributes:
+        queryset: The queryset of Planet objects to be listed
+        serializer_class: The serializer class to be used for serialization of the queryset
+        authentication_class: A tuple of authentication classes to be used fot API key authentication.
+        permission_classes: A tuple of permission classes to be used for user authentication
+    Methods:
+        get_queryset(): Returns the queryset of Planet objects to be listed based on the presence of an api_key
+        query parameter in the request URL
+    """
+    queryset = System.objects.all()
+    serializer_class = SystemSerializer
+    authentication_classes = (ApiKeyAuthentication, )
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_queryset(self):
+        api_key = self.request.query_params.get('api_key', None)
+        if api_key:
+            return System.objects.all()
+        else:
+            return None
+
