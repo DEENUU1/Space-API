e5be9f5035fe45197c76acc9d7b25b0459fb889d
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-03-23 18:09:04 +0100

    Feature | PlanetList | View proviedes a read-only list of all the Planet objects. Inherits from the ListAPIView class provied by Django Rest Framework endpoint for this view looks like this 'api/planets/?api_key=<<>>'

--- space_api/base/urls.py
+++ space_api/base/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import getRoutes
+from .views import getRoutes, PlanetList
 
 app_name = 'base'
 
 urlpatterns = [
     path('', getRoutes, name='get-routes'),
+    path('planets/', PlanetList.as_view(), name='planet-list'),
+
 ]
--- space_api/base/views.py
+++ space_api/base/views.py
@@ -16,16 +16,21 @@ def getRoutes(request):
     """
     routes = [
         {
-            'Endpoint': '/',
+            'Endpoint': 'api/planets/',
             'method': 'GET',
             'body': None,
-            'description': 'Some text'
+            'description': '?api_key=<<YOUR API KEY>>'
         },
     ]
     return Response(routes)
 
 
 class ApiKeyAuthentication(TokenAuthentication):
+    """
+    This authentication class extends the TokenAuthentication class
+    to allow authentication using API keys passed as query parameters
+    in the request URL.
+    """
     def authenticate(self, request):
         api_key = request.query_params.get('api_key')
         if not api_key:
@@ -35,4 +40,30 @@ class ApiKeyAuthentication(TokenAuthentication):
         except Token.DoesNotExist:
             raise AuthenticationFailed('Invalid API key')
 
-        return token.user, token
+        return (token.user, token)
+
+
+class PlanetList(generics.ListAPIView):
+    """
+    This view provides a read-only list of all the Planet objects.
+    Inherits from the ListAPIView class provided by DRF
+    Attributes:
+        queryset: The queryset of Planet objects to be listed
+        serializer_class: The serializer class to be used for serialization of the queryset
+        authentication_class: A tuple of authentication classes to be used fot API key authentication.
+        permission_classes: A tuple of permission classes to be used for user authentication
+    Methods:
+        get_queryset(): Returns the queryset of Planet objects to be listed based on the presence of an api_key
+        query parameter in the request URL
+    """
+    queryset = Planet.objects.all()
+    serializer_class = PlanetSerializer
+    authentication_classes = (ApiKeyAuthentication, )
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_queryset(self):
+        api_key = self.request.query_params.get('api_key', None)
+        if api_key:
+            return Planet.objects.all()
+        else:
+            return None
