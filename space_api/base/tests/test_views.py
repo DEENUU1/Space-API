3de41bc12b5cf4183c39fae0b189c050019b39a0
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-03-25 18:11:52 +0100

    Tests | SystemListTestCase | Tests the system list view with API key authentication Test the system list view without authentication Test the planet list view with an invalid API key BaseTestCase class which collect all data for testing

--- space_api/base/tests/test_views.py
+++ space_api/base/tests/test_views.py
@@ -7,12 +7,13 @@ from base.models import Planet, Galaxy, System
 from base.serializers import PlanetSerializer, SystemSerializer, GalaxySerializer
 
 
-class PlanetListTestCase(APITestCase):
+class BaseTestCase(APITestCase):
     @classmethod
     def setUp(cls):
         """
         Sets up the test data and creates a user, token, galaxy, system and planet objects for the test.
         """
+        super().setUpClass()
         cls.user = User.objects.create_user(
             username='test',
             password='test123')
@@ -28,6 +29,8 @@ class PlanetListTestCase(APITestCase):
                                            galaxy=cls.galaxy,
                                            system=cls.system)
 
+
+class PlanetListTestCase(BaseTestCase):
     def test_planet_list_with_api_key_authentication(self):
         """
         Tests the planet list view with API key authentication.
@@ -59,3 +62,39 @@ class PlanetListTestCase(APITestCase):
         url = reverse('base:planet-list')
         response = self.client.get(url, {'api_key': 'invalid_api_key'})
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class SystemListTestCase(BaseTestCase):
+    def test_system_list_with_api_key_authentication(self):
+        """
+        Tests the system list view with API key authentication.
+        It sends a GET request to the planet-list endpoint with a valid API key and expects a 200 OK response.
+        It also compares the response data with the expected data using the `SystemSerializer`.
+        """
+        url = reverse('base:system-list')
+        response = self.client.get(url, {'api_key': self.api_key})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        expected_data = SystemSerializer([self.system], many=True).data
+        self.assertEqual(response.data, expected_data)
+
+    def test_system_list_without_authentication(self):
+        """
+        Tests the system list view without authentication.
+        It sends a GET request to the system-list endpoint without an API key and expects a 401 UNAUTHORIZED response.
+        """
+        url = reverse('base:system-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_system_list_with_invalid_api_key(self):
+        """
+        Tests the system list view with an invalid API key.
+        It sends a GET request to the system-list endpoint with an invalid API key and expects a 401 UNAUTHORIZED
+        response.
+        """
+        url = reverse('base:system-list')
+        response = self.client.get(url, {'api_key': 'invalid_api_key'})
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
