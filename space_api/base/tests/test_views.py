70766f938529fef8ea1203d5e551a79a6625d0da
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-03-26 14:42:50 +0200

    Tests | PlanetListByGalaxyView | Test that planet list for a given galaxy can be retrived with valid api key, without api key and with invalid api key

--- space_api/base/tests/test_views.py
+++ space_api/base/tests/test_views.py
@@ -161,3 +161,42 @@ class SystemListByGalaxyTestCase(BaseTestCase):
         url = reverse('base:system-list-by-galaxy', args=[galaxy_id])
         response = self.client.get(url, {'api_key': 'invalid_api_key'})
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PlanetListByGalaxyTestCase(BaseTestCase):
+    def test_planet_list_by_galaxy_with_api_key_authentication(self) -> None:
+        """
+        Test that the planet list for a given galaxy can be retrieved with valid API key authentication.
+        This test sends a GET request to the 'planet-list-by-galaxy' endpoint with a valid API key for authentication.
+        The galaxy ID is set to 1, which is assumed to exist in the database.
+        The test checks that the response status code is HTTP_200_OK and that the response data contains one system.
+        """
+        galaxy_id = 1
+        url = reverse('base:planet-list-by-galaxy', args=[galaxy_id])
+        response = self.client.get(url, {'api_key': self.api_key})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+    def test_planet_list_by_galaxy_without_authentication(self) -> None:
+        """
+        Test that the planet list for a given galaxy cannot be retrieved without authentication.
+        This test sends a GET request to the 'planet-list-by-galaxy' endpoint without any authentication.
+        The galaxy ID is set to 1, which is assumed to exist in the database.
+        The test checks that the response status code is HTTP_401_UNAUTHORIZED.
+        """
+        galaxy_id = 1
+        url = reverse('base:planet-list-by-galaxy', args=[galaxy_id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_planet_list_by_galaxy_invalid_api_key(self) -> None:
+        """
+        Test that the planet list for a given galaxy cannot be retrieved with an invalid API key.
+        This test sends a GET request to the 'planet-list-by-galaxy' endpoint with an invalid API key for authentication
+        The galaxy ID is set to 1, which is assumed to exist in the database.
+        The test checks that the response status code is HTTP_401_UNAUTHORIZED.
+        """
+        galaxy_id = 1
+        url = reverse('base:planet-list-by-galaxy', args=[galaxy_id])
+        response = self.client.get(url, {'api_key': 'invalid_api_key'})
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
