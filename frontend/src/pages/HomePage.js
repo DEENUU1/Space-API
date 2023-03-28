e3e1f1c7593cf3093928a1273b408663ac943f36
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-03-28 23:28:49 +0200

    Style | Nagivation bar | Navigation bar using react bootstrap

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,9 +1,14 @@
 import React from "react";
+import NagationBar from '../components/NavigationBar';
 
 const HomePage = () => {
     return (
         <div>
-            <h1>Space API </h1>
+            <NagationBar/>
+            <div>
+                <h1>Space API </h1>
+                <a href='/planets'>List of planets</a>
+            </div>
         </div>
     )
 }
--- frontend/src/pages/PlanetsPage.js
+++ frontend/src/pages/PlanetsPage.js
@@ -1,36 +1,40 @@
 import React from "react";
 import { usePlanetsData } from '../hooks/planets.js'
+import NagationBar from '../components/NavigationBar';
 
 const PlanetsPage = () => {
-  const planets = usePlanetsData();
-
-  return (
-    <div>
-      <h1>Planets</h1>
-      <div>
-        {planets.map(planet => (
-          <div key={planet.name}>
-            <h3>{planet.name}</h3>
-            <p>{planet.description}</p>
-            <h4>More informations about this planet</h4>
-            <ul>
-              <li>Age: {planet.age}</li>
-              <li>Star: {planet.star}</li>
-              <li>Number of stars: {planet.number_of_stars}</li>
-              <li>Orbital period: {planet.orbital_period}</li>
-              <li>Setellites: {planet.setellites}</li>
-              <li>Mean radius: {planet.mean_radius}</li>
-              <li>Mass: {planet.mass}</li>
-              <li>Gravity on surface: {planet.surface_gravity}</li>
-              <li>Min temp: {planet.surface_temp_min}</li>
-              <li>Max temp: {planet.surface_temp_max}</li>
-              <li>Mean temp: {planet.surface_temp_mean}</li>
-            </ul>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+    const planets = usePlanetsData();
+    
+    return (
+        <div>
+        <NagationBar/>
+            <div>
+                <h1>Planets</h1>
+                <div>
+                    {planets.map(planet => (
+                        <div key={planet.name}>
+                            <h3>{planet.name}</h3>
+                            <p>{planet.description}</p>
+                            <h4>More information about this planet</h4>
+                            <ul>
+                                <li>Age: {planet.age}</li>
+                                <li>Star: {planet.star}</li>
+                                <li>Number of stars: {planet.number_of_stars}</li>
+                                <li>Orbital period: {planet.orbital_period}</li>
+                                <li>Satellites: {planet.setellites}</li>
+                                <li>Mean radius: {planet.mean_radius}</li>
+                                <li>Mass: {planet.mass}</li>
+                                <li>Gravity on surface: {planet.surface_gravity}</li>
+                                <li>Min temp: {planet.surface_temp_min}</li>
+                                <li>Max temp: {planet.surface_temp_max}</li>
+                                <li>Mean temp: {planet.surface_temp_mean}</li>
+                            </ul>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default PlanetsPage;
