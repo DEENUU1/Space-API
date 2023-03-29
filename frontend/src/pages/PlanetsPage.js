a314861b5b2044c31b0bb56a0a6ece4dc735f944
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-03-29 14:55:23 +0200

    Style | Bootstrap cards | Bootstrap cards for planets list page

--- frontend/src/pages/PlanetsPage.js
+++ frontend/src/pages/PlanetsPage.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { usePlanetsData } from '../hooks/PlanetList'
 import NagationBar from '../components/NavigationBar';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup'
 
 const PlanetsPage = () => {
     const planets = usePlanetsData();
@@ -9,30 +12,31 @@ const PlanetsPage = () => {
         <div>
         <NagationBar/>
             <div>
-                <h1>Planets</h1>
-                <div>
-                    {planets.map(planet => (
-                        <div key={planet.name}>
-                            <h3>{planet.name}</h3>
-                            <p>{planet.description}</p>
-                            <h4>More information about this planet</h4>
-                            <img src={planet.image}/>
-                            <ul>
-                                <li>Age: {planet.age}</li>
-                                <li>Star: {planet.star}</li>
-                                <li>Number of stars: {planet.number_of_stars}</li>
-                                <li>Orbital period: {planet.orbital_period}</li>
-                                <li>Satellites: {planet.setellites}</li>
-                                <li>Mean radius: {planet.mean_radius}</li>
-                                <li>Mass: {planet.mass}</li>
-                                <li>Gravity on surface: {planet.surface_gravity}</li>
-                                <li>Min temp: {planet.surface_temp_min}</li>
-                                <li>Max temp: {planet.surface_temp_max}</li>
-                                <li>Mean temp: {planet.surface_temp_mean}</li>
-                            </ul>
-                        </div>
-                    ))}
-                </div>
+                <h1 style={{textAlign: 'center'}}>Planets</h1>
+                {planets.map(planet => (
+                    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '20px'}}>
+                        <Card style={{ width: '30rem' }} className="text-center">
+                        <Card.Img variant="top" src={planet.image} />
+                        <Card.Body>
+                            <Card.Title>{planet.name}</Card.Title>
+                            <Card.Text>{planet.description}</Card.Text>
+                        </Card.Body>
+                        <ListGroup variant="flush">
+                            <ListGroup.Item>Age: {planet.age}</ListGroup.Item>
+                            <ListGroup.Item>Star: {planet.star}</ListGroup.Item>
+                            <ListGroup.Item>Number of stars: {planet.number_of_stars}</ListGroup.Item>
+                            <ListGroup.Item>Orbital period: {planet.orbital_period}</ListGroup.Item>
+                            <ListGroup.Item>Satellites: {planet.setellites}</ListGroup.Item>
+                            <ListGroup.Item>Mean radius: {planet.mean_radius}</ListGroup.Item>
+                            <ListGroup.Item>Mass: {planet.mass}</ListGroup.Item>
+                            <ListGroup.Item>Gravity on surface: {planet.surface_gravity}</ListGroup.Item>
+                            <ListGroup.Item>Min temp: {planet.surface_temp_min}</ListGroup.Item>
+                            <ListGroup.Item>Max temp: {planet.surface_temp_max}</ListGroup.Item>
+                            <ListGroup.Item>Mean temp: {planet.surface_temp_mean}</ListGroup.Item>
+                        </ListGroup>
+                        </Card>
+                    </div>  
+                ))}  
             </div>
         </div>
     );
