30581ada40f0e9632ba5a6de3820189608345827
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-03-29 22:55:38 +0200

    Feature | send email | Send email function which allows to send token by email after sign up

--- space_api/users/serializers.py
+++ space_api/users/serializers.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
+from .send_email import send_email
 
 
 class GetTokenSerializer(serializers.ModelSerializer):
     """
-    Docstring
+    This serializer class is a Django REST Framework serializer that is used to serialize user data to and from
+    JSON format. It includes a build in User model and the email field to be serialized.
     """
 
     class Meta:
@@ -14,11 +16,16 @@ class GetTokenSerializer(serializers.ModelSerializer):
 
     def create_user(self, validate_data):
         """
-        :param validate_data:
-        :return:
+        This` method creates a new user instance based on the validated data passed into it as
+        the `validate_data` parameter.
         """
         user = User(email=validate_data['email'])
         user.save()
         token = Token.objects.create(usesr=user)
-
+        send_email(
+            "Token",
+            "email_token.html",
+            token,
+            user.email
+        )
         return user
