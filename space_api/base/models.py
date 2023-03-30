a9e342f78991d1836d2e7b5e9ffe04910fbc2759
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-03-30 21:54:11 +0200

    Test | Model rocket and mission | Test case for models: rocket and mission which test creating a new object and relation between them

--- space_api/base/models.py
+++ space_api/base/models.py
@@ -69,6 +69,9 @@ class Rocket(models.Model):
     stages = models.IntegerField(blank=True, null=True)
     image = models.ImageField(upload_to="images", blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Mission(models.Model):
     id = models.AutoField(primary_key=True)
@@ -78,3 +81,6 @@ class Mission(models.Model):
     date_end = models.DateField(blank=True, null=True)
     rocket = models.ForeignKey(Rocket, on_delete=models.CASCADE)
     image = models.ImageField(upload_to="images", blank=True, null=True)
+
+    def __str__(self):
+        return self.name
