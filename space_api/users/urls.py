a14117927d860b3de5415947369baca315283a4b
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-03-30 16:08:37 +0200

    Feature | Delete account endpoint DeleteUserSerializer allows to make a POST request by giving email address and token to remove user's account. This endpoint requires a correct token and email

--- space_api/users/urls.py
+++ space_api/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import GetTokenView
+from .views import GetTokenView, DeleteTokenView
 
 app_name = 'users'
 
 urlpatterns = [
     path('token/', GetTokenView.as_view(), name='get-token'),
+    path('delete-token/', DeleteTokenView.as_view(), name='delete-token')
 
 ]
