6f440f52300618c497de12bdf7ecd2b5f05973f9
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-04-02 00:34:47 +0200

    Rect | POST request to create a new mission object | CreateMission is a hook which allows to create a new mission object MissionsPage allows to display a list of missions and has a form to create a new one

--- frontend/src/hooks/CreateMission.js
+++ frontend/src/hooks/CreateMission.js
@@ -3,14 +3,20 @@ import { useState } from 'react';
 export const useCreateMission = () => {
   const [error, setError] = useState(null);
 
-  async function getMission(image, name, description, date_start, date_end, rocket) {
+  async function createMission(image, name, description, dateStart, dateEnd, rocket) {
+    const formData = new FormData();
+    formData.append('image', image);
+    formData.append('name', name);
+    formData.append('description', description);
+    formData.append('date_start', dateStart);
+    formData.append('date_end', dateEnd);
+    formData.append('rocket', rocket);
+
     const response = await fetch('http://127.0.0.1:8000/api/mission/create?api_key=150af2b4256f7b9a3e0b68c6c6b92eb974cbef0c', {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ image, name, description, date_start, date_end, rocket }),
+      body: formData,
     });
+    
     if (response.ok) {
       setError(null);
     } else {
@@ -19,5 +25,5 @@ export const useCreateMission = () => {
     }
   }
 
-  return [error, getMission];
+  return [error, createMission];
 }
