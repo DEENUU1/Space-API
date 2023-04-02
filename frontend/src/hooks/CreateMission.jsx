dd1969c322f0f86be2e7a2a971a0353e549428cd
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-04-02 15:25:32 +0200

    React | Form to add new mission api key | Input field to add user's api key to create a new mission

--- frontend/src/hooks/CreateMission.jsx
+++ frontend/src/hooks/CreateMission.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 export const useCreateMission = () => {
   const [error, setError] = useState(null);
 
-  async function createMission(image, name, description, dateStart, dateEnd, rocket) {
+  async function createMission(image, name, description, dateStart, dateEnd, rocket, apiKey) {
     const formData = new FormData();
     formData.append('image', image);
     formData.append('name', name);
@@ -12,7 +12,7 @@ export const useCreateMission = () => {
     formData.append('date_end', dateEnd);
     formData.append('rocket', rocket);
 
-    const response = await fetch('http://127.0.0.1:8000/api/mission/create?api_key=150af2b4256f7b9a3e0b68c6c6b92eb974cbef0c', {
+    const response = await fetch(`http://127.0.0.1:8000/api/mission/create?api_key=${apiKey}`, {
       method: 'POST',
       body: formData,
     });
